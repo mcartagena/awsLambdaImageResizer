bb48d4f4d18e08f481ab98f7d992b8f6123eaa19
mcartagena <marcelocartagenaorellana@gmail.com>
2021-01-11 13:07:48 -0300

    adding testing functionality

--- src/test/java/com/mcartagena/imageresizer/LambdaFunctionHandlerTest.java
+++ src/test/java/com/mcartagena/imageresizer/LambdaFunctionHandlerTest.java
@@ -1,7 +1,15 @@
 package com.mcartagena.imageresizer;
 
 import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 
+import org.apache.http.client.methods.HttpRequestBase;
+
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 import org.junit.Assert;
@@ -20,25 +28,32 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
 /**
  * A simple test harness for locally invoking your Lambda function handler.
  */
 @RunWith(MockitoJUnitRunner.class)
-@Ignore
 public class LambdaFunctionHandlerTest {
 
     private final String CONTENT_TYPE = "image/jpeg";
+    private final String TEST_IMAGE_FILENAME = "HappyFace.jpg";
     private S3Event event;
 
     @Mock
     private AmazonS3 s3Client;
     @Mock
     private S3Object s3Object;
-
+    @Mock
+    private HttpRequestBase httpRequestBaseMock;
+    
     @Captor
     private ArgumentCaptor<GetObjectRequest> getObjectRequest;
 
+    @Captor
+    private ArgumentCaptor<PutObjectRequest> putObjectRequestCaptor;
+    
     @Before
     public void setUp() throws IOException {
         event = TestUtils.parse("/s3-event.put.json", S3Event.class);
@@ -48,6 +63,11 @@ public class LambdaFunctionHandlerTest {
         objectMetadata.setContentType(CONTENT_TYPE);
         when(s3Object.getObjectMetadata()).thenReturn(objectMetadata);
         when(s3Client.getObject(getObjectRequest.capture())).thenReturn(s3Object);
+
+        when(s3Object.getKey()).thenReturn(TEST_IMAGE_FILENAME);
+        
+        when(s3Object.getObjectContent()).thenReturn(new S3ObjectInputStream(LambdaFunctionHandler.class.getResourceAsStream("/HappyFace.jpg"), httpRequestBaseMock));
+    
     }
 
     private Context createContext() {
@@ -69,4 +89,40 @@ public class LambdaFunctionHandlerTest {
         // TODO: validate output here if needed.
         Assert.assertEquals(CONTENT_TYPE, output);
     }
+
+    @Test
+    public void testLambdaFunctionHandlerSendsResizedImageToTheResizedImageBucket() {
+        LambdaFunctionHandler handler = new LambdaFunctionHandler(s3Client);
+        Context ctx = createContext();
+
+        handler.handleRequest(event, ctx);
+        
+        // Verify that s3.putObject was called and capture the object used in the call
+        verify(s3Client).putObject(putObjectRequestCaptor.capture());
+        PutObjectRequest putObject = putObjectRequestCaptor.getValue();
+        
+        // Verify the parameter to putObject is what we expect
+        assertEquals("resizedimgmcartagena", putObject.getBucketName());
+        assertEquals(TEST_IMAGE_FILENAME, putObject.getKey());
+    }
+
+    
+    @Test
+    public void testLambdaFunctionHandlerResizedImageTo100x100() throws IOException {
+        LambdaFunctionHandler handler = new LambdaFunctionHandler(s3Client);
+        Context ctx = createContext();
+
+        handler.handleRequest(event, ctx);
+        
+        // Verify that s3.putObject was called and capture the object used in the call
+        verify(s3Client).putObject(putObjectRequestCaptor.capture());
+        PutObjectRequest putObjectRequest = putObjectRequestCaptor.getValue();
+        
+        // Get the image from the putObject call and verify that the image height and width is 100
+        File imageFile = putObjectRequest.getFile();
+		BufferedImage originalImage = ImageIO.read(imageFile);
+		assertEquals(100, originalImage.getHeight());
+		assertEquals(100, originalImage.getWidth());
+    }
+    
 }
